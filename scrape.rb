7b9fa24d816d6fd6c5af1c42e778e03b3f681d62
gusaaaaa <gusa@neo.com>
2014-04-09 12:15:05 -0300

    Updates the code according to the latest version of EasyParser gem

--- scrape.rb
+++ scrape.rb
@@ -18,21 +18,39 @@ ep_source = '
                 {...}
                 {many}
                   <table>
-                    <tbody>
-                      {many}
-                        <tr>
-                          <td>{$tipo}{/.*/}{/$tipo}</td>
-                          <td>{$denominacion}{/.*/}{/$denominacion}</td>
-                          <td>{$numero}{/.*/}{/$numero}</td>
-                          <td>{$fecha}{/.*/}{/$fecha}</td>
-                          <td>{$medio_y_fecha}{/.*/}{/$medio_y_fecha}</td>
-                          <td>{$ultimo_update}{/.*/}{/$ultimo_update}</td>
-                          <td>{$efectos}{/.*/}{/$efectos}</td>
-                          <td>{$descripcion}{/.*/}{/$descripcion}</td>
-                          {...}
-                        </tr>
-                      {/many}
-                    </tbody>
+                    {many}
+                      <tbody>
+                        {many}
+                          <tr>
+                            <td>{$tipo}{/.*/}{/$tipo}</td>
+                            {either}
+                              <td>
+                                {$denominacion}{/.*/}{/$denominacion}
+                              </td>
+                            {or}
+                              <td>
+                                <p>{$denominacion}{/.*/}{/$denominacion}</p>
+                              </td>
+                            {/either}
+                            <td>{$numero}{/.*/}{/$numero}</td>
+                            <td>{$fecha}{/.*/}{/$fecha}</td>
+                            {either}
+                              <td>
+                                {$medio_y_fecha}{/.*/}{/$medio_y_fecha}
+                              </td>
+                            {or}
+                              <td>
+                                <p>{$medio_y_fecha}{/.*/}{/$medio_y_fecha}</p>
+                              </td>
+                            {/either}
+                            <td>{$ultimo_update}{/.*/}{/$ultimo_update}</td>
+                            <td>{$efectos}{/.*/}{/$efectos}</td>
+                            <td>{$descripcion}{/.*/}{/$descripcion}</td>
+                            {...}
+                          </tr>
+                        {/many}
+                      </tbody>
+                    {/many}
                   </table>
                   {...but}<table>{...}</table>{/...but}
                 {/many}
@@ -49,10 +67,8 @@ ep_source = '
 
 easy_parser = EasyParser.new ep_source do |on|
   on.descripcion do |scope|
-    puts "#{scope['tipo']},#{scope['denominacion']},#{scope['numero']},#{scope['fecha']},#{scope['medio_y_fecha']},#{scope['ultimo_update']},#{scope['efectos']},\"#{scope['descripcion']}\""
+    puts "#{scope['tipo']},\"#{scope['denominacion']}\",#{scope['numero']},#{scope['fecha']},\"#{scope['medio_y_fecha']}\",\"#{scope['ultimo_update']}\",\"#{scope['efectos']}\",\"#{scope['descripcion']}\""
   end
 end
 
-result, scope = easy_parser.run File.read 'sample.html'
-# result, scope = easy_parser.run open('http://www.consejotransparencia.cl/actos-y-resoluciones-con-efectos-sobre-terceros/consejo/2012-12-18/195924.html')
-p result
+result, scope = easy_parser.run open('http://www.consejotransparencia.cl/actos-y-resoluciones-con-efectos-sobre-terceros/consejo/2012-12-18/195924.html')
